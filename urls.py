5e8533fef4e0660206ce3db2ab4ea1defb6778d7
Mauricio Quezada <waxkun@gmail.com>
2017-03-21 18:05:37 -0300

    threads

--- urls.py
+++ urls.py
@@ -58,7 +58,6 @@ def resolve_urls(urls: List[str], name: str, n_threads=10):
     exitFlag = False
     lock = Lock()
     q = Queue()
-    session = requests.session()
     expanded_urls = dict()
     total = len(urls)
     curr = 1
@@ -72,12 +71,12 @@ def resolve_urls(urls: List[str], name: str, n_threads=10):
 
     logger.info(f'Spawning {n_threads} threads')
     for _ in range(n_threads):
-        ur = URLResolver(q, expanded_urls, lock, session)
+        ur = URLResolver(q, expanded_urls, lock)
         ur.start()
         threads.append(ur)
 
     while not q.empty():
-        time.sleep(5)
+        pass
 
     logger.info(f'Done')
     exitFlag = True
@@ -94,3 +93,28 @@ def resolve_urls(urls: List[str], name: str, n_threads=10):
 
     logger.info("Exiting main thread")
     return expanded_urls
+
+
+
+
+
+if __name__ == 'test':
+    import sys
+
+    logging.basicConfig(format='%(asctime)s | %(levelname)s : %(message)s', level=logging.INFO, stream=sys.stdout)
+
+    event_ids = [13472, 15641, 15724, 15749, 15753, 15764, 15778, 15846, 15866, 15943, 15958, 15995, 15996, 16000,
+                 16024, 16032, 16034, 16109, 16111, 16145, 16186, 16277, 16292, 16347, 16695, 16700, 16702, 16703,
+                 16705, 16729, 16730, 16735, 17140, 17211, 17281, 17533, 17796, 17914, 18469, 18607, 18703, 19115,
+                 19119, 19127, 20376, 20415, 20421, 20425, 20441, 20444, 20466, 20556, 20573, 20576, 20582, 20612,
+                 20651, 20701, 20702, 20711, 20717, 20841, 20844, 20853, 20985, 20989, 21356, 21358, 21362, 21373,
+                 21381, 21495, 21501, 21512, 21514, 21642, 21687, 21733, 21782, 24164, 24165, 24179, 24185, 24198,
+                 24311, 24581, 24728, 25106, 25117, 25238, 25263, 25370, 25380, 25387, 25391, 26173, 26177, 26179,
+                 27024, 27026, 31659, 31749, 31750, 31753, 31759, 31892, 32033, 32233, 32624, 32699, 36246, 36253,
+                 36881, 36882, 36892, 36898, 36899, 36904, 36917, 37042, 37058]
+
+    from settings import engine_m3
+    urls = get_urls(event_ids, engine_m3)
+    urls = list(set(urls))
+
+    expanded_urls = resolve_urls(urls[:64], 'oscar pistorius')
